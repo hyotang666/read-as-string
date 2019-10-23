ea41834ce82fc15fc80004586b48e8617f19fa96
Shinichi Sato <hyotang.karakoma@gmail.com>
2019-10-23 18:41:53 +0900

    Cleanup: Remove unused codes.

--- read-as-string.lisp
+++ read-as-string.lisp
@@ -14,8 +14,6 @@
 (eval-when(:compile-toplevel :load-toplevel :execute)
   ;; See http://www.lispworks.com/documentation/HyperSpec/Body/02_ad.htm#charsyntaxtypesinstdsyntax
   (defvar *spaces* '(#\space #\newline #\tab #\page #\return #\linefeed)))
-(defvar *terminal-macro-chars* '(#\" #\' #\( #\) #\, #\; #\`))
-(defvar *terminals* (append *spaces* *terminal-macro-chars*))
 
 (declaim (ftype (function (&optional stream boolean t boolean)
 			  (or string t))
@@ -206,138 +204,6 @@
 	  (read-as-string stream t t t)
 	  (read-as-string stream t t t)))
 
-(defvar *parsers*(make-hash-table :test #'equal))
-
-(defun parser(cons &optional(default #'default-parser))
-  (gethash cons *parsers* default))
-
-(defun default-parser()
-  (Read-string-till (Delimiter *terminals*)))
-
-(eval-when(:load-toplevel :compile-toplevel :execute)
-  (defun upcase(cons)
-    (cons(car cons)(if(cdr cons)
-		     (char-upcase(cdr cons))
-		     (cdr cons))))
-
-  (defmacro defparser(cons &body body)
-    `(PROGN
-       (SETF(GETHASH ',cons *PARSERS*)
-	 (LAMBDA () ,@body))
-       (COPY-PARSER ',(upcase cons) ',cons))))
-
-(defun copy-parser(dest src)
-  (Propagatef(gethash src *parsers*)
-    (gethash dest *parsers*)
-    (gethash (upcase dest) *parsers*)))
-
-(defparser(#\space)
-  (concatenate 'string
-	       (Read-string-till(complement (Delimiter *spaces*)))
-	       (read-as-string *standard-input* T T T)))
-
-(progn . #.(map 'list(lambda(c)
-		       `(COPY-PARSER '(,c)'(#\space)))
-		*spaces*))
-
-(defparser(#\()
-  (loop :with open = 0
-	:with close = 0
-	:for c = (peek-char)
-	:if(char= #\( c)
-	:do(incf open):and :collect (read-char) :into result
-	:else :if(char= #\) c)
-	:do(incf close) :and :collect (read-char) :into result
-	:else :collect (read-as-string *standard-input* T T T)
-	:into result
-	:until(= open close)
-	:finally (return(String-concat result))))
-
-(defparser(#\`)
-  (concatenate 'string(string(read-char))
-	       (read-as-string *standard-input* T T T)))
-
-(progn . #.(mapcar(lambda(c)
-		    `(COPY-PARSER '(,c) '(#\`)))
-	     '(#\' #\,)))
-
-(defparser(#\")
-  (Read-delimited-string (read-char)))
-
-(progn . #.(mapcar (lambda(c)
-		     `(COPY-PARSER '(,c)'(#\")))
-		   '(#\|)))
-
-(defparser(#\;)
-  (Read-string-till (Char-pred #\newline) *standard-input* T T T T))
-
-(defparser(#\#)
-  (concatenate 'string "#"
-	       (funcall(parser (cons (read-char) ; consume #\#.
-				     (peek-char))
-			       (lambda() ; as default
-				 (concatenate 'string (string(read-char))
-					      (read-as-string *standard-input* T T T)))))))
-
-
-(defparser(#\# . #\\)
-  (uiop:strcat (read-char)
-	       (read-char)
-	       (unless(funcall(Delimiter *terminals*)(peek-char))
-		 (Read-string-till (Delimiter *terminals*)))))
-
-(defparser(#\# . #\()
-  (read-as-string *standard-input* T T T))
-
-(defparser(#\# . #\<)
-  (Read-string-till (Char-pred #\>) *standard-input* T T T T))
-
-(defparser(#\# . #\|)
-  (with-output-to-string(*standard-output*)
-    (write-char(read-char)) ; consume #\|
-    (loop (write-string(read-string-till (lambda(c)(find c '(#\| #\#)))))
-	  (let((c(read-char)) ; consume delimiter(i.e. #\| or #\#).
-	       (next(read-char)))
-	    (cond
-	      ((and (char= #\| c)
-		    (char= #\# next)) ; terminal.
-	       (write-char c)(write-char next)(return nil))
-	      ((and (char= #\# c)
-		    (char= #\| next)) ; nested.
-	       (write-char c)(unread-char next)
-	       (write-string (funcall(parser(cons c next))))) ; recurse.
-	      (t(write-char c)
-		(if(find next '(#\| #\#))
-		  (unread-char next)
-		  (write-char next))))))))
-
-(defparser(#\# . #\0)
-  (multiple-value-call #'uiop:strcat
-    (read-string-till(complement #'digit-char-p))
-    (let((c(read-char)))
-      (case c
-	((#\a #\A #\* #\= #\r #\R)
-	 (values c (read-as-string)))
-	((#\#) ; labelling.
-	 c)
-	(otherwise ; user defined dispatch macro character comes.
-	  (values c (read-as-string)))))))
-
-(progn . #.(map 'list(lambda(c)
-		       `(COPY-PARSER '(#\# . ,c) '(#\# . #\0)))
-		"123456789"))
-
-(defparser(#\# . #\+)
-  (apply #'concatenate 'string (string(read-char))
-	 (loop :with count = 0
-	       :for sexp = (read-as-string *standard-input* T T T)
-	       :collect sexp :into result
-	       :unless (commentp sexp)
-	       :do (incf count)
-	       :when (= 2 count)
-	       :do (loop-finish)
-	       :finally (return result))))
-
 (defun commentp(string)
   #+ecl(check-type string string)
   (labels((LINE-COMMENTP(string)
@@ -350,5 +216,3 @@
 				     (read-char s)))))))
     (or (LINE-COMMENTP string)
 	(BLOCK-COMMENTP string))))
-
-(copy-parser '(#\# . #\-) '(#\# . #\+))
